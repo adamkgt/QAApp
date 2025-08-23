61d6752a116a62daa82d04bf1a9501f2c7bb5531
Adam <73661260+adamkgt@users.noreply.github.com>
2025-08-23 08:32:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 // config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyA7qtIYm01pZdvY-5CNwOKVYgAIO0Vm25w",
-  authDomain: "tc-managment.firebaseapp.com",
-  projectId: "tc-managment",
-  storageBucket: "tc-managment.firebasestorage.app",
-  messagingSenderId: "514951940531",
-  appId: "1:514951940531:web:a6a2d9bf9c0e57e10923c6",
-  measurementId: "G-3VCJZWPJ2F"
+  apiKey: "TU_WSTAW_SWÓJ_API_KEY",
+  authDomain: "TU_WSTAW_SWÓJ_AUTH_DOMAIN",
+  projectId: "TU_WSTAW_SWÓJ_PROJECT_ID",
+  storageBucket: "TU_WSTAW_SWÓJ_STORAGE_BUCKET",
+  messagingSenderId: "TU_WSTAW_SWÓJ_MESSAGING_SENDER_ID",
+  appId: "TU_WSTAW_SWÓJ_APP_ID",
+  measurementId: "TU_WSTAW_SWÓJ_MEASUREMENT_ID"
 };
+
+// Inicjalizacja Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+const auth = firebase.auth();
+const db = firebase.firestore();
