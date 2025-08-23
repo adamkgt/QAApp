7d56ead3667ded23e57bea69e0d398fd17c0639e
Adam <73661260+adamkgt@users.noreply.github.com>
2025-08-23 08:11:17 +0200

    Update and rename config.template.js to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA7qtIYm01pZdvY-5CNwOKVYgAIO0Vm25w",
+  authDomain: "tc-managment.firebaseapp.com",
+  projectId: "tc-managment",
+  storageBucket: "tc-managment.firebasestorage.app",
+  messagingSenderId: "514951940531",
+  appId: "1:514951940531:web:a6a2d9bf9c0e57e10923c6",
+  measurementId: "G-3VCJZWPJ2F"
+};
